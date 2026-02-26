7d55471efcd811ceef9e78ace55719ce01a4aad2
jvrezendem <jrezendemarciano@gmail.com>
2026-02-26 11:04:12 -0300

    fix:content path

--- dashboards/app.py
+++ dashboards/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas
 from streamlit.components.v1 import html
+from pathlib import Path 
+
+
+BASE_DIR = Path(__file__).resolve().parent     
+ROOT_DIR = BASE_DIR.parent                      
+def p(*parts):                                  
+    return ROOT_DIR.joinpath(*parts)
+
 
 st.set_page_config(
     page_title="Focos de Incêncio no Brasil",
@@ -15,13 +23,13 @@ with col2:
 
     st.subheader("Gráficos")
 
-    st.image("../assets/fireBiomas.png", width=1600)
+    st.image(str(p("assets", "fireBiomas.png")), width=1600)
 
-    st.image("../assets/fireEstados.png", width=1600)
+    st.image(str(p("assets", "fireEstados.png")), width=1600)
 
     st.subheader("Mapas interativos")
 
-    mapPath = "../maps/heatMap.html"
+    mapPath = p("maps", "heatMap.html")
 
     with open(mapPath, "r", encoding="utf-8") as f:
         mapaHtml = f.read()
